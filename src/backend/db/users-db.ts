29c43f2947698a3e20ae0bea3bc58dd8749c1ccc
fichorka <fichorka@gmail.com>
2020-09-17 01:25:05 +0200

    Add findOneByUsername() to usersDb

--- src/backend/db/users-db.ts
+++ src/backend/db/users-db.ts
@@ -1,15 +1,22 @@
-import { User } from '../entities/user/user'
+import { User } from '../entities/types'
+import { UsersDb } from '../use-cases/types'
 
 export default function makeUsersDb({ makeDb, parseId }: Props): UsersDb {
     return {
         findAll,
         findOneById,
+        findOneByUsername,
         insertOne
     }
 
     async function findOneById(id: string): Promise<User> {
         const db = makeDb()
         return await db.collection('users').findOne({ _id: parseId(id) })
+    }
+
+    async function findOneByUsername(username: string) {
+        const db = await makeDb()
+        return await db.collection('users').findOne({ username })
     }
 
     async function findAll(): Promise<User[]> {
@@ -26,10 +33,4 @@ export default function makeUsersDb({ makeDb, parseId }: Props): UsersDb {
 interface Props {
     makeDb: CallableFunction
     parseId: CallableFunction
-}
-
-interface UsersDb {
-    findAll: CallableFunction
-    findOneById: CallableFunction
-    insertOne: CallableFunction
 }
--- src/backend/use-cases/types/index.ts
+++ src/backend/use-cases/types/index.ts
@@ -1,5 +1,6 @@
 export interface UsersDb {
     findAll: CallableFunction
     findOneById: CallableFunction
+    findOneByUsername: CallableFunction
     insertOne: CallableFunction
 }
