743f4c3466e12ae7a09f5538866bc11786b57576
fichorka <fichorka@gmail.com>
2020-09-18 02:48:48 +0200

    Export use-cases

--- src/backend/use-cases/index.ts
+++ src/backend/use-cases/index.ts
@@ -1,1 +1,12 @@
-export * from './user'
+import makeAddUser from './user/add-user'
+import makeEditUser from './user/edit-user'
+import makeListUsers from './user/list-users'
+import makeRemoveUser from './user/remove-user'
+import { usersDb } from '../db'
+
+const listUsers = makeListUsers({ usersDb })
+const addUser = makeAddUser({ usersDb })
+const editUser = makeEditUser({ usersDb })
+const removeUser = makeRemoveUser({ usersDb })
+
+export { listUsers, addUser, editUser, removeUser }
