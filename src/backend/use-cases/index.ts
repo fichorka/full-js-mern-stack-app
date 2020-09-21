bfdb0ce8a7dd64e74bc7807cfa85546622d9846c
fichorka <fichorka@gmail.com>
2020-09-21 15:34:31 +0200

    Add addItem use-case

--- src/backend/use-cases/index.ts
+++ src/backend/use-cases/index.ts
@@ -2,11 +2,14 @@ import makeAddUser from './user/add-user'
 import makeEditUser from './user/edit-user'
 import makeListUsers from './user/list-users'
 import makeRemoveUser from './user/remove-user'
-import { usersDb } from '../db'
+import { itemsDb, usersDb } from '../db'
+import { makeAddItem } from './item'
 
 const listUsers = makeListUsers({ usersDb })
 const addUser = makeAddUser({ usersDb })
 const editUser = makeEditUser({ usersDb })
 const removeUser = makeRemoveUser({ usersDb })
 
-export { listUsers, addUser, editUser, removeUser }
+const addItem = makeAddItem({ itemsDb })
+
+export { listUsers, addUser, editUser, removeUser, addItem }
---
+++ src/backend/use-cases/item/add-item.ts
@@ -0,0 +1,23 @@
+import { ItemsDb } from '../../db/types'
+import { Item } from '../../entities'
+import { makeItem } from '../../entities/item'
+
+const makeAddItem: MakeAddItem = function ({ itemsDb }) {
+    const addItem: AddItem = async function ({ itemInfo }) {
+        const item = makeItem({ itemInfo })
+        const exists = await itemsDb.findOneByName(item.name)
+        if (exists) {
+            throw new Error("Item's name is already taken.")
+        }
+        await itemsDb.insertOne(item)
+        return item
+    }
+
+    return addItem
+}
+
+export { makeAddItem }
+
+type MakeAddItem = ({ itemsDb }: { itemsDb: ItemsDb }) => AddItem
+
+type AddItem = ({ itemInfo }: { itemInfo: Item }) => Promise<Item>
